3d0ff2d63e42862d9e3ef18c00bd64fac0f7b2c4
Abhinav Namdeo <namedeoabhinav102@gmail.com>
2024-08-11 18:28:21 +0530

    complete sign in page functionality

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -79,7 +79,7 @@ function SignUp() {
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={"sign-in"}>
+        <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
