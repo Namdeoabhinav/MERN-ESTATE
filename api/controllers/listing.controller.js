91b811d786062ba182a511fe0674ee6604822231
Abhinav Namdeo <namedeoabhinav102@gmail.com>
2024-09-01 19:15:45 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -61,3 +61,52 @@ export const getListing = async(req , res , next) => {
         next(error);
     }
 }
+
+export const getListings = async(req , res , next) => {
+    const limit = parseInt(req.query.limit) || 9; //For seting the limit of the litings
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if(offer === undefined || offer == 'false'){
+        offer = { $in: [false , true]};
+    }
+
+    let furnished = req.query.furnished;
+
+    if(furnished === undefined || furnished == 'false'){
+        furnished = { $in: [false , true]};
+    }
+
+    let parking = req.query.parking;
+
+    if(parking === undefined || parking == 'false'){
+        parking = { $in: [false , true]};
+    }
+
+    let type = req.query.type;
+
+    if(type === undefined || type == 'all'){
+        type = { $in: ['sale' , 'rent']};
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    //regex is the built in search functionality in MongoDB.
+    //options: While searching it does not care about uppercase or lowercase
+    const listings = await Listing.find({
+        name:{ $regex: searchTerm , $options:'i'},
+        offer,
+        furnished,
+        parking,
+        type,
+    }).sort(
+        {[sort]:order}
+    ).limit(limit).skip(startIndex)
+
+    return res.status(200).json(listings);
+
+}
