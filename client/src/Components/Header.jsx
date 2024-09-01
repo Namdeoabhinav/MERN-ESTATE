5577404bb9ce7f6f0533c02fb7c8ff96b350c869
Abhinav Namdeo <namedeoabhinav102@gmail.com>
2024-09-01 19:46:14 +0530

    complete header search form functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link  , useNavigate} from "react-router-dom";
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const {currentUser} = useSelector(state => state.user);
+  const [searchTerm , setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //We have to show in the URL different queries we are using the inbuilt react method URLSearchParams
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  } , [location.search]);
+
   return (
     <div>
       <header className="bg-slate-200 shadow-md">
@@ -14,13 +34,17 @@ export default function Header() {
               <span className="text-slate-700">Ease</span>
             </h1>
           </Link>
-          <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
             <input
               type="text"
               placeholder="Search..."
               className="bg-transparent focus:outline-none w-24 sm:w-64"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
+            <button>
             <FaSearch className="text-slate-600" />
+            </button>
           </form>
 
           <ul className="flex gap-4">
